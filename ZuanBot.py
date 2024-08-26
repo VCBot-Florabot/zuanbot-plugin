5592d219d8fce6a39d7aff56c2522bdbda8ca62e
luyanci <2058282593@qq.com>
2024-08-26 18:24:04 +0800

    first commit

--- ZuanBot.py
+++ ZuanBot.py
@@ -1,3 +1,6 @@
+import random
+import json
+
 flora_api = {}  # 顾名思义,FloraBot的API,载入(若插件已设为禁用则不载入)后会赋值上
 
 
@@ -10,13 +13,14 @@ send_msg = occupying_function
 
 def init():  # 插件初始化函数,在载入(若插件已设为禁用则不载入)或启用插件时会调用一次,API可能没有那么快更新,可等待,无传入参数
     global send_msg
-    print(flora_api)
+    #print(flora_api)
     send_msg = flora_api.get("SendMsg")
-    print("FloraBot插件模板 加载成功")
+    print("ZuanBot 加载成功")
 
 
 def api_update_event():  # 在API更新时会调用一次(若插件已设为禁用则不调用),可及时获得最新的API内容,无传入参数
-    print(flora_api)
+    #print(flora_api)
+    pass
 
 
 def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用这个函数(若插件已设为禁用则不调用),传入原消息JSON参数
@@ -27,4 +31,27 @@ def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用
     msg = data.get("raw_message")  # 消息内容
     if msg is not None:
         msg = msg.replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", "&").replace("&#44;", ",")  # 消息需要将URL编码替换到正确内容
-        print(uid, gid, mid, msg)
+        #print(uid, gid, mid, msg)
+        if msg == "#祖安lite":
+            send_msg(msg=zuan_lite(), gid=gid,uid=uid)
+        if msg == "#祖安full":
+            send_msg(msg=zuan_full(), gid=gid,uid=uid)
+
+def zuan_lite():
+    #load zuan_lite.json
+    zuan_lite_list=json.load(open(f"./{flora_api.get("ThePluginPath")}/res/zuan-lite.json",mode="r",errors='ignore'))
+    num = make_random(len(zuan_lite_list))
+    print(zuan_lite_list[num]['index'])
+    return zuan_lite_list[num]['text']
+
+def zuan_full():
+    #load zuan_full.json
+    zuan_full_list=json.load(open(f"./{flora_api.get("ThePluginPath")}/res/zuan-full.json",mode="r",errors='ignore'))
+    num = make_random(len(zuan_full_list))
+    print(zuan_full_list[num]['index'])
+    return zuan_full_list[num]['text']
+
+
+def make_random(max:int):
+    print(max)
+    return random.randint(0,max)
