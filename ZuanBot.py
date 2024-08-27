dcdb719aa88894976b6b9948c389a70a42c23e9b
luyanci <2058282593@qq.com>
2024-08-27 14:39:01 +0800

    Compat v1.11-beta

--- ZuanBot.py
+++ ZuanBot.py
@@ -19,7 +19,7 @@ def init():  # 插件初始化函数,在载入(若插件已设为禁用则不载
 
 
 def api_update_event():  # 在API更新时会调用一次(若插件已设为禁用则不调用),可及时获得最新的API内容,无传入参数
-    #print(flora_api)
+    print(flora_api)
     pass
 
 
@@ -29,16 +29,27 @@ def event(data: dict):  # 事件函数,FloraBot每收到一个事件都会调用
     gid = data.get("group_id")  # 事件对象群号
     mid = data.get("message_id")  # 消息ID
     msg = data.get("raw_message")  # 消息内容
+    try:
+        global ws_client
+        global ws_server
+        send_address = data.get("SendAddress")
+        
+        ws_client = send_address.get("WebSocketClient")
+        ws_server = send_address.get("WebSocketServer")
+    except:
+        ws_client=None
+        ws_server=None
+        pass
     if msg is not None:
         msg = msg.replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", "&").replace("&#44;", ",")  # 消息需要将URL编码替换到正确内容
-        #print(uid, gid, mid, msg)
+        print(uid, gid, mid, msg)
         if gid is not None and msg in ["#祖安lite","#祖安full"]:
-            send_msg(msg="请在私聊环境中使用",gid=gid,uid=uid)
+            send_compatible(msg="请在私聊环境中使用",gid=gid,uid=uid,data=data)
             return
         if msg == "#祖安lite":
-            send_msg(msg=zuan_lite(), gid=gid,uid=uid)
+            send_compatible(msg=zuan_lite(), gid=gid,uid=uid,data=data)
         if msg == "#祖安full":
-            send_msg(msg=zuan_full(), gid=gid,uid=uid)
+            send_compatible(msg=zuan_full(), gid=gid,uid=uid,data=data)
 
 def zuan_lite():
     #load zuan_lite.json
@@ -58,3 +69,12 @@ def zuan_full():
 def make_random(max:int):
     print(max)
     return random.randint(0,max)
+
+def send_compatible(msg:str,gid:str|int,uid: str|int,data: dict=None):  #兼容性函数,用于兼容旧版本API
+    if flora_api.get("FloraVersion") == 'v1.01':
+        send_msg(msg=msg,gid=gid,uid=uid)
+    else:
+        send_type=flora_api.get("ConnectionType")
+        send_address=flora_api.get("FrameworkAddress")
+        send_msg(msg=msg,gid=gid,uid=uid,send_type=send_type,ws_client=ws_client,ws_server=ws_server)
+        
